ff098041468a5f09e52a0e6363fc7956a52820a6
python240419 <51232248+python240419@users.noreply.github.com>
2019-07-05 12:10:38 +0300

    Create dictionary_1.py

---
+++ dictionary_1.py
@@ -0,0 +1,19 @@
+
+# dictionary :
+# create dictionary i.e. { 1 : 'moshe', 2: 'erez', 3: 'dana' }
+# create function - parameters gets dictionary, key
+#  retrun True if key exist and False if not exist
+
+def doesKeyExist(d, k):
+    '''
+    checks if key exists in dictionary
+    :param d: dictionary
+    :param k: key
+    :return: True if k exist in d
+    '''
+    #
+    #
+    #list(d.keys())
+    #return True;
+
+myDict = { 1 : 'moshe', 2: 'erez', 3: 'dana' }
