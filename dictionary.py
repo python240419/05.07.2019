3ebfc6654b62613f05b50fe3388b1a97b00af133
python240419 <51232248+python240419@users.noreply.github.com>
2019-07-05 11:46:21 +0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+
+# Dictionary
+# Hashmap -- key : value
+# key may appear only once - single appearence
+#12 -- moshe
+#14 -- moshe
+#12 -- danny
+# 2 keys have the same value
+#
+#
+#   3,4,5             moshe, danny, tamar
+#
+# key - may be anything
+
+# [1,2,3]  #adress 4005
+# [1,2,3]
+
+#<id>3 <name> itay </name> </id> # XML
+#{ key : value, key : value }
+a = {'a' : 'b', 'c' : { 'd' : 'e' }}
+# create dictionary for cities population
+# TLV, LONDON, PARIS, TOKYO
+# input city name -- print population
+
+
+
+
+
