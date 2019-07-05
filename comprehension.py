7483f983b7a877299fe911446351a25cc863a2c9
python240419 <51232248+python240419@users.noreply.github.com>
2019-07-05 10:13:07 +0300

    Update comprehension.py

--- comprehension.py
+++ comprehension.py
@@ -9,7 +9,8 @@ result = []
 
 for word in listofWords:
     result.append(word[0])
-    
+
+print(word) # word still exists
 
 print(result)
 
@@ -22,9 +23,22 @@ numbers = [4, 10, -5, 200]
 # multiply each item by 3
 # ignore negative numbers
 
+numbers3 = [x*3 for x in numbers if x > 0]
+print(numbers3)
+
+# print(x) -- error
+
 matrix = [[1,2,6], [7, 8, 10], [100, 5, 2]]
 # create a list of first item in each list
 
+# one row
+rows = [oneList[0] for oneList in matrix]
+
+# all rows
+# matrix = [[1,2,6], [7], [100, 5, 2]]
+for i in range(len(matrix)):
+    rows = [oneList[i] for oneList in matrix if i < len(oneList)]
+    print(rows)
 
 
 
